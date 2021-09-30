249514310f8ae6735b2e573c29a55d247cbe0263
Jeff Tian <jeff.tian@outlook.com>
2021-09-30 20:23:58 +0800

    feat: map fetch to Taro.request

--- src/pages/yuque/index.tsx
+++ src/pages/yuque/index.tsx
@@ -1,23 +1,39 @@
-import {ApolloClient, gql, InMemoryCache} from "@apollo/client"
+import {ApolloClient, gql, HttpLink, InMemoryCache} from "@apollo/client"
 import {Component} from 'react'
 import {View, Text} from "@tarojs/components"
+import Taro from '@tarojs/taro'
 
-const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
-  cache: new InMemoryCache()
-})
+export default class YuQue extends Component {
 
-client.query({
-  query: gql`
+  constructor(props) {
+    super(props)
+
+    const client = new ApolloClient({
+      link: new HttpLink({
+        uri: 'https://48p1r2roz4.sse.codesandbox.io', fetch: async (url) => {
+          const res = await Taro.request({
+            url: url.toString(),
+            success: console.log
+          })
+
+          console.log('res = ', res);
+
+          return {text: async () => JSON.stringify({hello: 'world'})} as any
+        }
+      }),
+      cache: new InMemoryCache()
+    })
+
+    client.query({
+      query: gql`
     query GetRates {
         rates(currency: "USD") {
           currency
         }
       }
   `
-}).then(console.log)
-
-export default class YuQue extends Component {
+    }).then(console.log)
+  }
 
   render() {
     return <View><Text>YuQue Blog</Text></View>
