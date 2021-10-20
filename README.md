94c0a22ab54b579ec6991e33c3a8d327bd0f31d8
Jeff Tian <jeff.tian@outlook.com>
2021-10-20 15:14:56 +0800

    chore: move picture out of src

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-weapp A WeChat Helper
+weapp
 
 ---
 
 [简体中文](README_zh-CN.md)
 
-> jump to WeChat miniprogram's deeplink quickly
+> Personal Wechat Miniprogram for Hardway
 
 |Trial Version|Release Version|
 | --- | --- |
-|![哈德韦体验版](basicprofile.jpeg)| ToDo |
+|![哈德韦体验版](basicprofile.jpeg)| ![哈德韦正式版](./扫码_搜索联合传播样式-微信标准绿版.png) |
 
 Local development
 
--- src/pages/weapp/index.tsx
+++ src/pages/weapp/index.tsx
@@ -1,10 +1,9 @@
 import {Image} from "@tarojs/components"
 
 import HardwayLayout from "../layout/hardway-layout"
-import weappPng from './扫码_搜索联合传播样式-微信标准绿版.png'
 
 const Weapps = () => <HardwayLayout>
-  <Image className='at-article__img' src={weappPng} mode='widthFix' />
+  <Image className='at-article__img' src='https://raw.githubusercontent.com/Jeff-Tian/weapp/main/扫码_搜索联合传播样式-微信标准绿版.png' mode='widthFix' />
 </HardwayLayout>
 
 export default Weapps
