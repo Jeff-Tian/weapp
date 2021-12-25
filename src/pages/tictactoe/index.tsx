b1e6c22c6a55a0ef8f29d21b2372330c4e0cf3e3
Jeff Tian@Workstation <jie.tian@live.cn>
2021-12-25 17:20:42 +0800

    feat: render button

--- src/pages/tictactoe/index.tsx
+++ src/pages/tictactoe/index.tsx
@@ -2,35 +2,44 @@ import { View } from "@tarojs/components"
 import { Interpreter } from "eval5";
 import TaroDOM from '@tarojs/react'
 import ReactDOM from 'react-dom'
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Taro, { ENV_TYPE } from "@tarojs/taro";
+import './tictactoe.styl'
 
 const interpreter = new Interpreter(window, {
   timeout: 1000,
 });
 
-const TicTacToe = () => {
-  window['ReactDOM'] = ENV_TYPE.WEB === Taro.getEnv() ? ReactDOM : TaroDOM;
+console.log('this = ', this, global, window);
 
+window['ReactDOM'] = ENV_TYPE.WEB === Taro.getEnv() ? ReactDOM : TaroDOM;
+// window['React'] = ENV_TYPE.WEB === Taro.getEnv() ? Nerv : React;
+window['React'] = React;
+
+const TicTacToe = () => {
   const [dynamicContent, setDynamicContent] = useState('')
 
   useEffect(() => {
     const dynamicContentRenderring = `
-    var ReactDOM = this.ReactDOM;
-    console.log('findDomNode = ', ReactDOM.findDOMNode, document.getElementById('react-dom-view'))
-    ReactDOM.render('hello', document.getElementById('react-dom-view'))
+    console.log(this)
+    ReactDOM.render(helloWorld(), document.getElementById('react-dom-view'))
+
+    function helloWorld (){
+      return React.createElement('button');
+    }
   `;
     const res = interpreter.evaluate(dynamicContentRenderring)
 
     console.log('res = ', res, '; d = ', dynamicContent)
 
     setDynamicContent(res)
-  }, [dynamicContent])
+  }, [])
 
   return <View>
     以下是动态渲染的内容：
     <View id='react-dom-view'>
     </View>
+    <View id='root'></View>
   </View>
 }
 
