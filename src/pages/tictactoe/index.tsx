4677fae09bce191cabdee559cec53d2ac359640d
Jeff Tian@Workstation <jie.tian@live.cn>
2021-12-25 09:24:06 +0800

    feat: try render by reactdom

--- src/pages/tictactoe/index.tsx
+++ src/pages/tictactoe/index.tsx
@@ -1,7 +1,7 @@
-import {View} from "@tarojs/components"
-import {Interpreter} from "eval5";
-import Taro from '@tarojs/taro'
-import React from 'react';
+import { View } from "@tarojs/components"
+import { Interpreter } from "eval5";
+import Taro, { onWindowResize } from '@tarojs/taro'
+import React, { useEffect } from 'react';
 import ReactDOM from '@tarojs/react'
 import ENV_TYPE = Taro.ENV_TYPE;
 
@@ -13,17 +13,30 @@ const TicTacToe = () => {
   global.React = React;
   window['ReactDOM'] = ReactDOM;
   console.log('This = ', this, global, window)
-  const res = ENV_TYPE.WEB === Taro.getEnv() ? interpreter.evaluate(`
+  console.log(ReactDOM.render);
+  console.log('view = ', window.document.querySelector('div'))
+
+  let res
+  useEffect(() => {
+    const query = Taro.createSelectorQuery();
+    const test = query.select('#test');
+    console.log('test = ', test)
+
+
+
+    res = ENV_TYPE.WEB === Taro.getEnv() ? interpreter.evaluate(`
     console.log('this = ', this, '${global.React}')
     var React = this.React;
     console.log(React.createElement);
     React.createElement('div', null, 'Hello');
   `) : interpreter.evaluate(`
     var ReactDOM = this.ReactDOM;
-    ReactDOM.render('Hello');
+    console.log('findDomNode = ', ReactDOM.findDOMNode, document.getElementById('test'))
+    ReactDOM.render('hello', document.getElementById('test'))
   `)
+  }, [])
 
-  return <View>
+  return <View id="test">
     以下是动态渲染的内容：
     {res}
   </View>
