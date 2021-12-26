eff1657b3b496a3d463edc4edf9c1b1e03705120
Jeff Tian <jeff.tian@outlook.com>
2021-12-26 16:34:23 +0800

    refactor

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,23 +1,26 @@
-import {View} from "@tarojs/components"
+import { View } from "@tarojs/components"
 import { Interpreter } from "eval5";
 import Taro from '@tarojs/taro'
-import {useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import HardwayLayout from "../layout/hardway-layout"
 
 import packageJson from '../../../package.json'
 import SystemInfo from "./system.info";
 
-const interpreter = new Interpreter(window, {
-  timeout: 1000,
-});
-const res = interpreter.evaluate(`1+1`)
-
-console.log('res = ', res)
 const About = () => {
   const [code, setCode] = useState('')
-  useEffect(()=>{
-    Taro.request({url:'https://pearlsplus.pa-ca.me/test.js?abc=def', responseType: 'text',dataType: 'text/plain', header: {'Content-Type': 'text/plain'}}).then(data=>{
+  useEffect(() => {
+
+    const interpreter = new Interpreter(window, {
+      timeout: 1000,
+    });
+    const res = interpreter.evaluate(`1+1`)
+
+    console.log('res = ', res)
+
+
+    Taro.request({ url: 'https://pearlsplus.pa-ca.me/test.js?abc=def', responseType: 'text', dataType: 'text/plain', header: { 'Content-Type': 'text/plain' } }).then(data => {
       console.log('data = ', data)
       setCode(data.data)
       console.log(code)
--- src/pages/tictactoe/index.tsx
+++ src/pages/tictactoe/index.tsx
@@ -9,7 +9,7 @@ import './tictactoe.styl'
 import divviewer from "../../adapters/divviewer";
 
 // eslint-disable-next-line import/no-commonjs
-const Babel = require('@babel/standalone/babel.min.js');
+// const Babel = require('@babel/standalone/babel.min.js');
 
 const interpreter = new Interpreter(window, {
   timeout: 1000,
@@ -18,81 +18,22 @@ const interpreter = new Interpreter(window, {
 console.log('this = ', this, global, window);
 
 window['ReactDOM'] = ENV_TYPE.WEB === Taro.getEnv() ? ReactDOM : TaroDOM;
-// window['React'] = ENV_TYPE.WEB === Taro.getEnv() ? Nerv : React;
 window['React'] = React;
 
 const TicTacToe = () => {
   const [dynamicContent, setDynamicContent] = useState('')
 
   useEffect(() => {
-    const tictactoe = `class Square extends React.Component {
-  render() {
-    return /*#__PURE__*/(
-      React.createElement("button", { className: "square" }));
+    Taro.request({ url: 'https://tictactoets.pa-ca.me/src/Game.tsx', responseType: 'text', dataType: 'text/plain' }).then(data => {
+      const tictactoe = data.data;
 
+      // Babel.registerPlugin('divviewer', divviewer);
+      // const output = Babel.transform(tictactoe, { presets: ['env'], plugins: [] }).code.replace(/"div"/g, '"view"').replace(/"ol"/g, '"view"')
 
+      const output = tictactoe;
+      console.log('output = ', output);
 
-  }}
-
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return /*#__PURE__*/React.createElement(Square, null);
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("div", { className: "status" }, status), /*#__PURE__*/
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(0),
-      this.renderSquare(1),
-      this.renderSquare(2)), /*#__PURE__*/
-
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(3),
-      this.renderSquare(4),
-      this.renderSquare(5)), /*#__PURE__*/
-
-      React.createElement("div", { className: "board-row" },
-      this.renderSquare(6),
-      this.renderSquare(7),
-      this.renderSquare(8))));
-
-
-
-  }}
-
-
-class Game extends React.Component {
-  render() {
-    return /*#__PURE__*/(
-      React.createElement("div", { className: "game" }, /*#__PURE__*/
-      React.createElement("div", { className: "game-board" }, /*#__PURE__*/
-      React.createElement(Board, null)), /*#__PURE__*/
-
-      React.createElement("div", { className: "game-info" }, /*#__PURE__*/
-      React.createElement("div", null), /*#__PURE__*/
-      React.createElement("ol", null))));
-
-
-
-  }}
-
-
-// ========================================
-
-ReactDOM.render( /*#__PURE__*/
-React.createElement(Game, null),
-document.getElementById('root'));`
-
-    Babel.registerPlugin('divviewer', divviewer);
-    const output = Babel.transform(tictactoe, {presets: ['env'], plugins: []}).code.replace(/"div"/g, '"view"').replace(/"ol"/g, '"view"')
-    console.log('output = ', output);
-
-    const dynamicContentRenderring = `
+      const dynamicContentRenderring = `
     console.log(this)
     ReactDOM.render(helloWorld(), document.getElementById('react-dom-view'))
 
@@ -102,11 +43,12 @@ document.getElementById('root'));`
 
     ${output}
   `;
-    const res = interpreter.evaluate(dynamicContentRenderring)
+      const res = interpreter.evaluate(dynamicContentRenderring)
 
-    console.log('res = ', res, '; d = ', dynamicContent)
+      console.log('res = ', res, '; d = ', dynamicContent)
 
-    setDynamicContent(res)
+      setDynamicContent(res)
+    })
   }, [])
 
   return <View>
