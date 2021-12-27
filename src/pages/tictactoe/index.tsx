b07e565c181ac09dd6f130667c77703f37de6600
Jeff Tian <jeff.tian@outlook.com>
2021-12-27 20:00:54 +0800

    feat: more loading effect

--- src/pages/tictactoe/index.tsx
+++ src/pages/tictactoe/index.tsx
@@ -4,15 +4,14 @@ import TaroDOM from '@tarojs/react'
 import ReactDOM from 'react-dom'
 import React, {useEffect, useState} from "react";
 import Taro, {ENV_TYPE} from "@tarojs/taro";
-import './tictactoe.styl'
 import {AtActivityIndicator} from "taro-ui";
+import './tictactoe.styl'
+
 // import divviewer from "../../adapters/divviewer";
 
 const interpreter = new Interpreter(window, {
   timeout: 5000,
 });
-
-console.log('this = ', this, global, window);
 
 window['ReactDOM'] = ENV_TYPE.WEB === Taro.getEnv() ? ReactDOM : TaroDOM;
 window['React'] = React;
@@ -40,13 +39,27 @@ function buildBabel(res: Taro.request.SuccessCallbackResult<any>) {
   global['Babel'] = interpreter.evaluate(`${babel}; window.Babel`)
 }
 
-const fetchJsAsPlainText = url => Taro.request({
-  url: `https://uniheart.pa-ca.me/proxy?url=${encodeURIComponent(url)}`, responseType: 'text', dataType: 'text/plain'
-})
-
 const TicTacToe = () => {
+  const fetchJsAsPlainText = (url: string) => {
+    setUrls([...urls, url])
+
+    return Taro.request({
+      url: `https://uniheart.pa-ca.me/proxy?url=${encodeURIComponent(url)}`,
+      responseType: 'text',
+      dataType: 'text/plain'
+    }).then(x => {
+      const index = urls.indexOf(url)
+
+      setUrls([...urls.slice(0, index), ...urls.slice(index + 1)])
+
+      return x
+    });
+  }
+
   useEffect(() => {
     Promise.all([fetchJsAsPlainText('https://unpkg.com/@babel/standalone@7.16.6/babel.min.js'), fetchJsAsPlainText('https://raw.githubusercontent.com/Jeff-Tian/TicTacToeTs/main/src/Game.tsx')]).then(([babel, tictactoe]) => {
+      setUrls([])
+
       buildBabel(babel)
       sanitizeAndRender(tictactoe)
 
@@ -55,11 +68,13 @@ const TicTacToe = () => {
   }, [])
 
   const [loading, setLoading] = useState(true)
+  const [urls, setUrls] = useState<string[]>([])
 
   return <View>
     以下是动态渲染的内容：
     <View id='react-dom-view'>
-      <AtActivityIndicator mode='center' size={128} content='加载中……'
+      <AtActivityIndicator mode='normal' size={50} content={`加载 ${urls.join('\n')} 中……`} isOpened={urls.length > 0} />
+      <AtActivityIndicator mode='center' size={128} content='渲染中……'
         isOpened={loading}
       />
     </View>
