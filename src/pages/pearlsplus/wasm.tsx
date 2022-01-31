ebeb2966cea25e2791e62d7bbcbec6cce888e007
Jeff Tian@Workstation <jie.tian@live.cn>
2022-01-31 18:31:19 +0800

    wip: eval5 for glew js

--- src/pages/pearlsplus/wasm.tsx
+++ src/pages/pearlsplus/wasm.tsx
@@ -1,23 +1,33 @@
 import Taro from '@tarojs/taro'
 import { View } from "@tarojs/components"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HardwayLayout from "../layout/hardway-layout";
+import './wasm.styl'
 
 const PearlsPlus = () => {
+  const [output, setOutput] = useState('')
+
   useEffect(() => {
     const worker = Taro.createWorker('pages/wasm/index.js')
-    worker.postMessage({ msg: 'msg from main' });
     worker.onMessage(({ message }) => {
-      Taro.showToast({
-        title: String(message)
-      })
+      if (typeof message === 'string') {
+        Taro.showToast({
+          title: String(message)
+        })
+      } else {
+        console.log('worker said: ', message)
+        setOutput(message.output)
+      }
     })
   }, [])
 
 
   return <HardwayLayout><View>
     以下是动态渲染的内容：
-    {/*{res}*/}
+    <View id='status'></View>
+    <View id='progress'></View>
+    <View id='spinner'></View>
+    <View className='terminal' id='output'>{output}</View>
   </View></HardwayLayout>
 }
 
