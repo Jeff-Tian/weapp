a7078e65ea4c6990185ad38203953b21c71dce8f
Jeff Tian@Workstation <jie.tian@live.cn>
2022-06-05 16:54:56 +0800

    fix: yarn dev:h5 errors

---
+++ src/pages/subpages/pearlsplus/detail.config.ts
@@ -0,0 +1,5 @@
+export default {
+  navigationBarTitleText: '程序详情',
+  enableShareAppMessage: true,
+  enableShareTimeline: true
+}
---
+++ src/pages/subpages/pearlsplus/index.config.ts
@@ -0,0 +1,5 @@
+export default {
+  navigationBarTitleText: '编程珠玑',
+  enableShareAppMessage: true,
+  enableShareTimeline: true
+}
