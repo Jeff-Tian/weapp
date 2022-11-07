c6e3c6b067c75a3cfae26b7eac97b1bb8b9a7edc
cnjeftia <jeff.tian@outlook.com>
2022-11-07 09:26:32 +0800

    refactor: move helper functions into a dedicate file

--- scripts/deeplinks.js
+++ scripts/deeplinks.js
@@ -1,30 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-
-const curry = fn => {
-  const arity = fn.length;
-
-  return function $curry(...args) {
-    if (args.length < arity) {
-      return $curry.bind(null, ...args);
-    }
-
-    return fn.call(null, ...args)
-  }
-}
-
-const filter = curry((f, a) => a.filter(f))
-const endsWith = curry((e, s) => s.endsWith(e))
-const replace = curry((a, b, s) => s.replace(a, b))
-const prepend = curry((a, b) => `${a}${b}`)
-const append = curry((a, b) => `${b}${a}`)
-const map = curry((f, a) => a.map(f))
-const compose = (...fns) => (...args) => fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0]
-
-const filterByExtension = compose(filter, endsWith)
-const prop = propName => obj => obj[propName]
-const head = arr => arr[0]
-const tail = arr => arr[arr.length - 1]
+const {compose, map, replace, prepend, append, head, filterByExtension, tail} = require("./helpers");
 
 function getAllDeepLinks(parent = '../src/pages') {
   const subFolders = fs.readdirSync(path.join(__dirname, parent)).filter(thePath => fs.statSync(path.join(__dirname, parent, thePath)).isDirectory())
---
+++ scripts/helpers.js
@@ -0,0 +1,25 @@
+
+export const curry = fn => {
+  const arity = fn.length;
+
+  return function $curry(...args) {
+    if (args.length < arity) {
+      return $curry.bind(null, ...args);
+    }
+
+    return fn.call(null, ...args)
+  }
+}
+
+export const filter = curry((f, a) => a.filter(f))
+export const endsWith = curry((e, s) => s.endsWith(e))
+export const replace = curry((a, b, s) => s.replace(a, b))
+export const prepend = curry((a, b) => `${a}${b}`)
+export const append = curry((a, b) => `${b}${a}`)
+export const map = curry((f, a) => a.map(f))
+export const compose = (...fns) => (...args) => fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0]
+
+export const filterByExtension = compose(filter, endsWith)
+export const prop = propName => obj => obj[propName]
+export const head = arr => arr[0]
+export const tail = arr => arr[arr.length - 1]
