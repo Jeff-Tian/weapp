67e452ec14940562b7564a5a58be700ae77389af
cnjeftia <jeff.tian@outlook.com>
2022-11-11 18:37:02 +0800

    fix: only show publish button when logged in

--- src/pages/yuque/article.tsx
+++ src/pages/yuque/article.tsx
@@ -1,7 +1,7 @@
 import {gql, useMutation, useQuery} from "@apollo/client"
 import {Button, Image, View} from "@tarojs/components"
 import {AtActivityIndicator, AtBadge, AtButton} from "taro-ui"
-import Taro, {onCopyUrl} from "@tarojs/taro"
+import Taro from "@tarojs/taro"
 
 import remark from 'remark'
 import remarkHtml from "remark-html"
@@ -11,9 +11,11 @@ import {SYNC_YUQUE_TO_ZHIHU, draftDirectly} from "@/services/zhihu";
 import './article.styl'
 
 import HardwayLayout from "../../layout/hardway-layout"
+import {StorageKeys} from "@/common/constants";
 
 
 const YuQueArticle: React.FC = () => {
+  const zhihuUserInfo = Taro.getStorageSync(StorageKeys.zhihuUserInfo)
   const params = Taro.getCurrentInstance()?.router?.params
 
   assert.ok(params, "本页必须传递参数！")
@@ -84,7 +86,7 @@ const YuQueArticle: React.FC = () => {
   })
 
   return <HardwayLayout><AtActivityIndicator mode='center' size={128} content='加载中……'
-    isOpened={loading}
+                                             isOpened={loading}
   />
     {data && data.yuque && <View className='at-article'>
       {data.yuque.cover && <Image
@@ -98,7 +100,7 @@ const YuQueArticle: React.FC = () => {
         {data.yuque.title}
         <AtBadge>
           <AtButton size='small'
-            onClick={() => Taro.setClipboardData({data: `pages/yuque/article?slug=${slug}`})}
+                    onClick={() => Taro.setClipboardData({data: `pages/yuque/article?slug=${slug}`})}
           >拷贝本页路径</AtButton>
         </AtBadge>
       </View>
@@ -106,11 +108,13 @@ const YuQueArticle: React.FC = () => {
         {data.yuque.created_at}&nbsp;&nbsp;&nbsp;{data.yuque.word_count} 字
       </View>
 
-      <Button onClick={() => slug ? syncYuqueToZhihu() : draftDirectly(data.yuque.title, html)}>发布到知乎</Button>
+      {zhihuUserInfo &&
+        <Button onClick={() => slug ? syncYuqueToZhihu() : draftDirectly(data.yuque.title, html)}>发布到知乎</Button>
+      }
 
       <View className='at-article__content taro_html'>
         <View className='at-article__section'>
-          <View dangerouslySetInnerHTML={{__html: html}} />
+          <View dangerouslySetInnerHTML={{__html: html}}/>
         </View>
       </View>
 
