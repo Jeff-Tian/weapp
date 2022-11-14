d779dc30496ae909bd52879bdb970cbd45ffc748
cnjeftia <jeff.tian@outlook.com>
2022-11-14 19:08:10 +0800

    feat: add logout feature UNI-28

--- src/pages/subpages/auth/profile.tsx
+++ src/pages/subpages/auth/profile.tsx
@@ -1,7 +1,19 @@
-import {AtAvatar} from "taro-ui";
+import {AtAvatar, AtGrid} from "taro-ui";
+import {View} from "@tarojs/components";
+import Taro from "@tarojs/taro";
 
 const Profile = () => {
-  return <AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
+  return <View><AtAvatar circle image='https://jdc.jd.com/img/200'></AtAvatar>
+    <AtGrid data={[{image: '', value: '退出登录'}]} onClick={(_item, index) => {
+      if (index === 0) {
+        Taro.clearStorage().then(() => {
+          Taro.reLaunch({url: 'pages/yuque/index'})
+        })
+      }
+    }
+    }
+    />
+  </View>
 }
 
 export default Profile;
