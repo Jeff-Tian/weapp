e13ce56e42e3ed44c20191035e7837201f10df47
cnjeftia <jeff.tian@outlook.com>
2022-12-03 17:58:02 +0800

    feat: jump to article if read yuque link from clipboard

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,31 @@
 import {ApolloProvider} from '@apollo/client'
 import {Component} from 'react'
+import Taro from "@tarojs/taro"
 
 import 'taro-ui/dist/style/index.scss'
 
 import './app.styl'
 import {client} from "./apollo-client"
 
+
+function handleYuQueUrl(url: string) {
+  console.log('url = ', url);
+  if (url.indexOf(`yuque.com`) >= 0) {
+    const slug = url.split(`/`).slice(-1)[0]
+    console.log('slug = ', slug)
+    if (slug) {
+      Taro.navigateTo({url: `/pages/yuque/article?slug=${slug.replace(/"/g, '')}`})
+    }
+  } else {
+    console.log('rul = ', url);
+  }
+}
+
+Taro.getClipboardData({
+  success: res => {
+    handleYuQueUrl(res.data)
+  }
+})
 
 class App extends Component {
   render() {
