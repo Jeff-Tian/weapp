89fb44cea09e0037f0caa0033a99308d6a0d48ed
cnjeftia <jeff.tian@outlook.com>
2023-01-06 18:13:21 +0800

    feat: friend list by page UNI-88

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -5,8 +5,8 @@ import {View} from "@tarojs/components";
 export const UserCard = ({userInfo}: { userInfo: User | undefined }) => {
   return userInfo ?
     <AtCard note={`注册于 ${userInfo.createdAt}，最近于 ${userInfo.lastLogin} 登录过`}
-            extra={userInfo.phone || undefined}
-            title={userInfo.username || userInfo.nickname || userInfo.email || undefined}
-            thumb={userInfo.photo || undefined}
+      extra={userInfo.phone || undefined}
+      title={userInfo.username || userInfo.nickname || userInfo.email || undefined}
+      thumb={userInfo.photo || undefined}
     >共登录过 {userInfo.loginsCount} 次。</AtCard> : <View>未获取到信息</View>
 }
--- src/pages/subpages/friends/list.tsx
+++ src/pages/subpages/friends/list.tsx
@@ -2,16 +2,24 @@ import SinglePageLayout from "@/layout/single-page-layout";
 import {UserCard} from "@/components/UserCard";
 import {gql, useQuery} from "@apollo/client";
 import {authingAppId} from "@/common/constants";
+import {useEffect, useState} from "react";
+import {AtLoadMore} from "taro-ui";
+import {View} from "@tarojs/components";
 
 const QUERY = gql`
-  query GetFriendList {
-    friendList (appId: "${authingAppId}") {
+  query GetFriendList($page: Int!, $pageSize: Int!) {
+    friendListByPage (appId: "${authingAppId}", page: $page, pageSize: $pageSize) {
       list {
         name
         email
         nickname
         userId
         username
+        lastLogin
+        phone
+        photo
+        loginsCount
+
       }
       totalCount
     }
@@ -19,7 +27,31 @@ const QUERY = gql`
 `
 
 const FriendList = () => {
-  const {loading, error, data} = useQuery(QUERY, {})
+  const [page, setPage] = useState(1)
+  const [pageSize] = useState(5)
+  const [status, setStatus]: ['more' | 'loading' | 'noMore' | undefined, Function] = useState('loading')
+
+  const [friends, setFriends] = useState([])
+
+  const {loading, error, data, refetch} = useQuery(QUERY, {variables: {page, pageSize}})
+
+  const loadMore = () => {
+    setPage(page + 1)
+    setStatus('loading')
+    refetch({page: page + 1, pageSize}).then()
+  }
+
+  useEffect(() => {
+    if (data) {
+      if (data.friendListByPage.list.length < pageSize) {
+        setStatus('noMore')
+      } else {
+        setStatus('more')
+      }
+
+      setFriends(friends.concat(data.friendListByPage.list))
+    }
+  }, [data])
 
   if (loading) {
     return <SinglePageLayout>Loading...</SinglePageLayout>
@@ -28,9 +60,11 @@ const FriendList = () => {
   if (error) {
     return <SinglePageLayout>Error: {error.message}</SinglePageLayout>
   }
-  console.log('data = ', data)
+
   return <SinglePageLayout>
-    {data.friendList.list.map(f => <UserCard key={f.userId} userInfo={f} />)}
+    <View>一共有 {data.friendListByPage.totalCount} 位朋友</View>
+    {friends.map(f => <UserCard key={f.userId} userInfo={f}/>)}
+    <AtLoadMore onClick={loadMore} status={status}/>
   </SinglePageLayout>
 }
 
