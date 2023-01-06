5a1540f8b00b28cacd29d66bef2a109e883e087c
cnjeftia <jeff.tian@outlook.com>
2023-01-06 16:00:59 +0800

    feat: query friendlist UNI-88

--- src/pages/subpages/friends/list.tsx
+++ src/pages/subpages/friends/list.tsx
@@ -1,12 +1,36 @@
 import SinglePageLayout from "@/layout/single-page-layout";
-import {useState} from "react";
 import {UserCard} from "@/components/UserCard";
+import {gql, useQuery} from "@apollo/client";
+import {authingAppId} from "@/common/constants";
+
+const QUERY = gql`
+  query GetFriendList {
+    friendList (appId: "${authingAppId}") {
+      list {
+        name
+        email
+        nickname
+        userId
+        username
+      }
+      totalCount
+    }
+  }
+`
 
 const FriendList = () => {
-  const [friends, setFriends] = useState([])
+  const {loading, error, data} = useQuery(QUERY, {})
 
+  if (loading) {
+    return <SinglePageLayout>Loading...</SinglePageLayout>
+  }
+
+  if (error) {
+    return <SinglePageLayout>Error: {error.message}</SinglePageLayout>
+  }
+  console.log('data = ', data)
   return <SinglePageLayout>
-    {friends.map(f => <UserCard userInfo={f}/>)}
+    {data.friendList.list.map(f => <UserCard key={f.userId} userInfo={f} />)}
   </SinglePageLayout>
 }
 
