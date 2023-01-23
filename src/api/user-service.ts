3b3b82a1efcff1514db835508cf8ee8eda126a14
Jeff Tian@Workstation <jie.tian@live.cn>
2023-01-23 22:12:36 +0800

    feat(UNI-80): save zhihu user info to user service

--- src/api/user-service.ts
+++ src/api/user-service.ts
@@ -1,5 +1,7 @@
 import {gql} from "@apollo/client";
 import {client} from "@/apollo-client";
+import Taro from "@tarojs/taro";
+import {StorageKeys} from "@/common/constants";
 
 const SAVE_PREFERENCE_MUTATION = gql`
 mutation SaveMyZhihuCookies ($key: String, $value: String) {
@@ -11,11 +13,17 @@ mutation SaveMyZhihuCookies ($key: String, $value: String) {
 `
 
 export const saveMyZhihuCookies = (cookieData: string) => {
-  return client.mutate({
+  return Promise.all([client.mutate({
     mutation: SAVE_PREFERENCE_MUTATION,
     variables: {
       key: 'zhihu-user-cookie',
       value: cookieData
     }
-  })
+  }), client.mutate({
+    mutation: SAVE_PREFERENCE_MUTATION,
+    variables: {
+      key: StorageKeys.zhihuUserInfo,
+      value: Taro.getStorageSync(StorageKeys.zhihuUserInfo)
+    }
+  })])
 }
