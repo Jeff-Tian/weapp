d39352a4d864dc8182d1a92d93fa8a3a8adc7400
cnjeftia <jeff.tian@outlook.com>
2023-01-31 19:02:54 +0800

    test(UNI-123): createPortal can be used

--- src/pages/subpages/test/index.tsx
+++ src/pages/subpages/test/index.tsx
@@ -1,8 +1,21 @@
 import {Button, RootPortal, View} from "@tarojs/components"
 import {useState} from "react";
+import {createPortal} from "react-dom";
+
+function ModalContent({ onClose }) {
+  return (
+    <div className='modal'>
+      <div>I'm a modal dialog</div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
 
 const Test = () => {
   const [show, setShow] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+
   const toggle = () => {
     setShow(!show)
   }
@@ -12,7 +25,15 @@ const Test = () => {
     <Button onClick={toggle}>显示root-portal</Button>
     {
       show && (<RootPortal><View>content</View></RootPortal>)
-    }</View>
+    }
+    <button onClick={() => setShowModal(true)}>
+      Show modal using a portal
+    </button>
+    {showModal && createPortal(
+      <ModalContent onClose={() => setShowModal(false)} />,
+      document.body
+    )}
+  </View>
 }
 
 export default Test
