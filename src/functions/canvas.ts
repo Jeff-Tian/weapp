c744c1d225f8d95e3ae9aacf7a89d40fd6b5fc0a
cnjeftia <jeff.tian@outlook.com>
2023-02-13 13:39:27 +0800

    feat(UNI-109): add sticker making part

--- src/functions/canvas.ts
+++ src/functions/canvas.ts
@@ -3,7 +3,10 @@ import {naiveErrorHandler} from "@/functions/naiveErrorHandler";
 
 export const drawImageFully = async (imagePath, ctx: CanvasContext, canvasId) => {
   const query = Taro.createSelectorQuery();
-  query.select('#' + canvasId).boundingClientRect(({height: canvasHeight, width: canvasWidth}) => {
+  query.select('canvas[canvas-id=' + canvasId + ']').boundingClientRect(canvas => {
+    console.log('canvas = ', canvas, ', imagePath = ', imagePath);
+    const {width: canvasWidth, height: canvasHeight} = canvas;
+
     Taro.getImageInfo({src: imagePath}).then(({width: imageWidth, height: imageHeight}) => {
       const ratio = imageWidth / imageHeight;
       console.log('ratio = ', ratio, canvasWidth, imageWidth, canvasHeight, imageHeight);
--- src/pages/subpages/sticker/index.tsx
+++ src/pages/subpages/sticker/index.tsx
@@ -1,15 +1,48 @@
 import SinglePageLayout from "@/layout/single-page-layout";
-import {View} from "@tarojs/components";
+import {Canvas, View} from "@tarojs/components";
 import LinkedImage from "@/components/LinkedImage";
+import {AtButton} from "taro-ui";
+import Taro, {CanvasContext, useReady} from "@tarojs/taro";
+import {naiveErrorHandler} from "@/functions/naiveErrorHandler";
+import {drawImageFully} from "@/functions/canvas";
 
 const Sticker = () => {
+  let context: CanvasContext;
+  useReady(() => {
+    context = Taro.createCanvasContext('sticker-canvas')
+  })
+  const chooseImage = () => {
+    Taro.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+    }).then((res) => {
+      console.log('files = , ', res);
+      const [image1] = res.tempFiles
+
+      return drawImageFully(image1.path, context, 'sticker-canvas').then(() => {console.log('drawn')})
+    }).catch(naiveErrorHandler);
+  };
+
   return <SinglePageLayout>
     <View className='at-article'>
-      <View className='at-article__h1'>点击下图进入预览模式，然后长按识别二维码（或者保存到本地然后扫一扫），领取微信表情：</View>
+      <View
+        className='at-article__h1'
+      >点击下图进入预览模式，然后长按识别二维码（或者保存到本地然后扫一扫），领取微信表情：</View>
       <LinkedImage
         src='https://i2.lensdump.com/i/TiEvVm.png'
         mode='widthFix'
       />
+    </View>
+    <View>
+      <View className='at-article__h1'>你也可以制作！</View>
+      <View className='at-article__content'>
+        上传图片，自动裁剪成微信表情尺寸。
+      </View>
+      <AtButton onClick={chooseImage}>选择照片</AtButton>
+      <View>
+        <Canvas canvasId='sticker-canvas' id='sticker-canvas' style='width: 300px; height: 300px;'></Canvas>
+      </View>
     </View>
   </SinglePageLayout>
 }
