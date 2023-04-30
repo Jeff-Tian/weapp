258153d917fe29c86f776009bbf87d32c4293aea
cnjeftia <jeff.tian@outlook.com>
2023-04-30 15:33:07 +0800

    feat: add token to profile page

--- src/common/token.ts
+++ src/common/token.ts
@@ -1,13 +1,19 @@
 import Taro from "@tarojs/taro";
-import {authing, guard} from "@/common/authing";
+import { authing, guard } from "@/common/authing";
 
 export const getToken = async () => {
   if (Taro.getEnv() === Taro.ENV_TYPE.WEAPP) {
     const user = await authing.getCurrentUser();
     return user?.token;
   } else {
-    const user = await guard.checkLoginStatus();
+    const loginStatus = await guard.checkLoginStatus();
 
-    return user?.token;
+    if (loginStatus.data.id) {
+      const user = await guard.trackSession();
+
+      return user.token;
+    } else {
+      return null;
+    }
   }
-}
+};
--- src/pages/subpages/auth/profile.tsx
+++ src/pages/subpages/auth/profile.tsx
@@ -1,4 +1,4 @@
-import {AtButton, AtDivider, AtForm, AtInput} from "taro-ui";
+import {AtButton, AtDivider, AtForm, AtInput, AtTextarea} from "taro-ui";
 import Taro, {ENV_TYPE} from "@tarojs/taro";
 import SinglePageLayout from "@/layout/single-page-layout";
 import {useEffect, useState} from "react";
@@ -8,44 +8,146 @@ import {WebLoginStatus} from "@/components/LoginStatus/web";
 import {getUserInfo, logout} from "@/common/login";
 import {User} from "@authing/guard-react";
 import MyZhihu from "@/components/zhihu/MyZhihu";
-import './profile.styl'
+import {Label} from "@tarojs/components";
+import {getToken} from "@/common/token";
 
 const Profile = () => {
   const [userInfo, setUserInfo] = useState<User | null>(null);
+  const [token, setToken] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    getUserInfo().then(setUserInfo)
-  }, [])
-  return <SinglePageLayout>
-    {Taro.getEnv() === ENV_TYPE.WEAPP ? <WeappLoginStatus /> : <WebLoginStatus />}
+    getUserInfo().then(setUserInfo);
+  }, []);
 
-    <AtDivider />
-    <AtForm>
-      <AtInput name='createdAt' title='注册日期' type='text' placeholder='请输入注册日期' value={userInfo?.createdAt ?? ''}
-        disabled
-      />
-      <AtInput name='phone' title='手机号' type='text' placeholder='请输入手机号' value={userInfo?.phone} disabled />
-      <AtInput name='email' title='邮箱' type='text' placeholder='请输入邮箱' value={userInfo?.email} disabled />
+  useEffect(() => {
+    getToken().then(setToken);
+  }, []);
+  return (
+    <SinglePageLayout>
+      {Taro.getEnv() === ENV_TYPE.WEAPP ? (
+        <WeappLoginStatus />
+      ) : (
+        <WebLoginStatus />
+      )}
+
       <AtDivider />
-      <AtInput name='username' title='用户名' type='text' placeholder='请输入用户名' value={userInfo?.username}
-        disabled
-      />
-      <AtInput name='name' title='姓名' type='text' placeholder='请输入姓名' value={userInfo?.name} disabled />
-      <AtInput name='nickname' title='昵称' type='text' placeholder='请输入昵称' value={userInfo?.nickname} disabled />
-      <AtInput name='address' title='地址' type='text' placeholder='请输入地址' value={userInfo?.address} disabled />
-      <AtInput name='gender' title='性别' type='text' placeholder='请输入性别' value={userInfo?.gender} disabled />
-      <AtInput name='birthday' title='生日' type='text' placeholder='请输入生日' value={userInfo?.birthdate} disabled />
-      <AtInput name='company' title='公司' type='text' placeholder='请输入公司' value={userInfo?.company} disabled />
-    </AtForm>
-    <AtDivider />
+      <AtForm>
+        <AtInput
+          name='createdAt'
+          title='注册日期'
+          type='text'
+          placeholder='请输入注册日期'
+          value={userInfo?.createdAt ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='phone'
+          title='手机号'
+          type='text'
+          placeholder='请输入手机号'
+          value={userInfo?.phone ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='email'
+          title='邮箱'
+          type='text'
+          placeholder='请输入邮箱'
+          value={userInfo?.email ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtDivider />
+        <AtInput
+          name='username'
+          title='用户名'
+          type='text'
+          placeholder='请输入用户名'
+          value={userInfo?.username ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='name'
+          title='姓名'
+          type='text'
+          placeholder='请输入姓名'
+          value={userInfo?.name ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='nickname'
+          title='昵称'
+          type='text'
+          placeholder='请输入昵称'
+          value={userInfo?.nickname ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='address'
+          title='地址'
+          type='text'
+          placeholder='请输入地址'
+          value={userInfo?.address ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='gender'
+          title='性别'
+          type='text'
+          placeholder='请输入性别'
+          value={userInfo?.gender ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='birthday'
+          title='生日'
+          type='text'
+          placeholder='请输入生日'
+          value={userInfo?.birthdate ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <AtInput
+          name='company'
+          title='公司'
+          type='text'
+          placeholder='请输入公司'
+          value={userInfo?.company ?? ""}
+          disabled
+          onChange={() => {
+          }}
+        />
+        <Label>个人令牌：</Label>
+        {token && <AtTextarea value={token} onChange={() => {
+        }} disabled maxLength={10000} placeholder='个人令牌'
+        />}
+      </AtForm>
+      <AtDivider />
 
-    {userInfo && <MyZhihu />}
+      {userInfo && <MyZhihu />}
 
-    <AtDivider></AtDivider>
-    <AtButton onClick={logout}
-      type='secondary'
-    >退出登录</AtButton>
-  </SinglePageLayout>
-}
+      <AtDivider></AtDivider>
+      <AtButton onClick={logout} type='secondary'>
+        退出登录
+      </AtButton>
+    </SinglePageLayout>
+  );
+};
 
 export default Profile;
