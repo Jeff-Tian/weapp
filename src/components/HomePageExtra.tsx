65e24ee1b0e0a018171f2e44f516a6d4c4a1a4f8
Jeff Tian <jeff.tian@outlook.com>
2024-02-23 19:49:43 +0800

    feat: remove inproper items

--- src/components/HomePageExtra.tsx
+++ src/components/HomePageExtra.tsx
@@ -1,10 +1,7 @@
 import {AtAvatar} from "taro-ui";
-import {Swiper, SwiperItem, View, Image} from "@tarojs/components";
-import OfficialAccountWrapper from "@/components/OfficialAccountWrapper";
+import {Swiper, SwiperItem, View} from "@tarojs/components";
 import StackOverflowWrapper from "@/components/StackOverflowWrapper";
-import LinkedImage from "@/components/LinkedImage";
 import {handleClipboard} from "@/functions/clipboard";
-import Taro from "@tarojs/taro";
 
 export const Fab = () => <View className='fab-area' onClick={handleClipboard}>
   <AtAvatar circle image='https://avatars.githubusercontent.com/u/3367820?v=4'></AtAvatar>
@@ -18,28 +15,9 @@ export const Banner = () => <Swiper
   indicatorDots
   autoplay
 >
-  <SwiperItem onClick={() => Taro.navigateTo({url: '/pages/subpages/brickverse/index'})}>
-    <Image src='https://i.lensdump.com/i/TVjKjm.md.jpeg' mode='widthFix' />
-  </SwiperItem>
-  <SwiperItem onClick={() => Taro.navigateTo({url: '/pages/subpages/sticker/index'})}>
-    <Image
-      src='https://i3.lensdump.com/i/TiELvi.jpeg'
-      mode='widthFix'
-    />
-  </SwiperItem>
   <SwiperItem>
     <StackOverflowWrapper />
   </SwiperItem>
-  <SwiperItem
-    onClick={() => Taro.navigateTo({url: `/pages/subpages/react-view/webview?src=${encodeURIComponent('https://mp.weixin.qq.com/s/yjlyzp_XGFeRG9Su7X418A')}`})}
-  >
-    <OfficialAccountWrapper />
-  </SwiperItem>
-  <SwiperItem>
-    <LinkedImage src='https://picd.zhimg.com/v2-246288a8f323592fed53cc347aaa65c9_r.jpg?source=172ae18b'
-      href='https://picd.zhimg.com/v2-246288a8f323592fed53cc347aaa65c9_r.jpg?source=172ae18b'
-      mode='widthFix'
-    />
-  </SwiperItem>
+
 </Swiper>
 
--- src/layout/drawer-items.ts
+++ src/layout/drawer-items.ts
@@ -4,29 +4,18 @@ export const drawerItems = new Map<string, () => void>([
   ['博文', async () => Taro.navigateTo({
     url: '/pages/yuque/index',
   })],
-  ['brickverse', async () => Taro.navigateTo({
-    url: '/pages/subpages/brickverse/index',
-  })],
-  ['React 井字棋游戏初始状态', async () => Taro.navigateTo({url: '/pages/subpages/tictactoe/index'})],
-  ['React 井字棋游戏最终状态', async () => Taro.navigateTo({url: '/pages/subpages/tictactoe/index2'})],
-  ['人工智能版井字棋游戏', async () => Taro.navigateTo({url: '/pages/subpages/tictactoe/ai'})],
-  ['小程序版的 C++ Hello World', async () => Taro.navigateTo({url: '/pages/subpages/pearlsplus/hello'})],
-  ['编程珠玑习题', async () => Taro.navigateTo({url: '/pages/subpages/pearlsplus/index'})],
-  ['动态渲染', async () => Taro.navigateTo({url: '/pages/subpages/react-view/index'})],
-  ['动态加载资源', async () => Taro.navigateTo({url: '/pages/subpages/react-view/browser'})],
-  ['小程序直达器',
-    async () => Taro.navigateTo({url: '/pages/index/index'})],
-  // ['ip 地址', async () => Taro.navigateTo({url: '/pages/ip/index'})],
+  ['井字棋游戏', async () => Taro.navigateTo({url: '/pages/subpages/tictactoe/ai'})],
   ['关于', async () => Taro.navigateTo({url: '/pages/subpages/about/index'})],
-  ['人脸识别', async () => Taro.navigateTo({url: '/pages/subpages/face-swap/landmark'})],
   ['哈德韦的朋友们', async () => Taro.navigateTo({url: '/pages/subpages/friends/list'})],
-  ['红包🧧封面', async () => Taro.navigateTo({url: '/pages/subpages/red-package/dragon'})],
   ['微信表情', async () => Taro.navigateTo({url: '/pages/subpages/sticker/index'})],
-  ['资源', async () => Taro.navigateTo({url: '/pages/subpages/video/index'})],
   // ['测试', async () => Taro.navigateTo({url: '/pages/subpages/test/index'})],
 ])
 
 if (Taro.getEnv() === Taro.ENV_TYPE.WEB) {
+  drawerItems.set('brickverse', async () => Taro.navigateTo({
+    url: '/pages/subpages/brickverse/index',
+  }))
+
   drawerItems.set('换脸', async () => Taro.navigateTo({url: '/pages/subpages/face-swap/index'}))
 
   drawerItems.set('向我咨询', async () => window.open('https://www.zhihu.com/consult/people/1073548674713423872'))
