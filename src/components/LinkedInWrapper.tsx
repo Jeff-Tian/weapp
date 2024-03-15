46f4fe6f81877e98ffc9379861474770bd0b63f0
Jeff Tian <jeff.tian@outlook.com>
2024-03-15 18:46:38 +0800

    feat(UNI-75): change domain

--- src/components/LinkedInWrapper.tsx
+++ src/components/LinkedInWrapper.tsx
@@ -2,7 +2,7 @@ import Wrapper from "@/common/wrapper";
 import {Image} from "@tarojs/components";
 
 const WeappLinkedInImage = () => <Image className='at-article__img' mode='scaleToFill'
-  src='https://first-go-vercel.vercel.app/api/dynamicimage?url=https://resume.jijiyy.me/zh-CN/jeff-tian/linked-in&width=332&height=242'
+  src='https://go.inversify.cn/api/dynamicimage?url=https://resume.jijiyy.me/zh-CN/jeff-tian/linked-in&width=332&height=242'
 />
 const H5LinkedInImage = () => <iframe src='https://resume.jijiyy.me/linkedin/jeff~tian.html'
   style={{border: 'none', width: '100%', height: '315px'}}
